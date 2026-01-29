fc0d8aca47b8274ebe0bf5c916695fc4a88451d5
shubham <shubhamsinghmor2312@gmail.com>
2026-01-29 20:05:56 +0530

    done

--- backend/controller/init.js
+++ backend/controller/init.js
@@ -1,11 +1,17 @@
 import fs from 'fs/promises'; 
 import path from "path";
-export function initRepo(){
+export async function initRepo(){
    const repoPath = path.resolve(process.cwd() ,".mygit")
    const commitsPath = path.join(repoPath,"commits");
     try{
 
-        
+        await fs.mkdir(repoPath,{ recursive: true })
+        await fs.mkdir(commitsPath,{ recursive: true })
+        await fs.writeFile(
+            path.join(repoPath,"config.json"),
+            JSON.stringify({ bucket:process.env.S3_BUCKET})
+        );
+        console.log("Initialized empty MyGit repository in", repoPath);
     }catch(err){
         console.error("Error initializing repository:", err);
     }

13366ddbc336d0e5daa3b58c0c109eea11391c66
shubham <shubhamsinghmor2312@gmail.com>
2026-01-30 22:24:19 +0530

    done

--- backend/controller/add.js
+++ backend/controller/add.js
@@ -1,3 +1,7 @@
+import fs from 'fs/promises';
+import path from "path";
 export async function add(){
+    const repoPath = path.resolve(process.cwd(),".mygit")
+    const stagingPath=path.join(repoPath,"staging")
     console.log("File added to the repository")
 }

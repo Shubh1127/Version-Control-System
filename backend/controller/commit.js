1437615aaaed645dde44d403a0e4f1a457ca1d80
shubham <shubhamsinghmor2312@gmail.com>
2026-01-31 05:20:12 +0530

    done

--- backend/controller/commit.js
+++ backend/controller/commit.js
@@ -1,3 +1,27 @@
-export async function commit(){
-    console.log("Changes committed to the repository")
+import fs from 'fs/promises';
+import path from "path";
+import { v4 as uuidv4 } from 'uuid'
+export async function commit(message){
+    const repoPath = path.resolve(process.cwd(),".mygit")
+    const stagingPath=path.join(repoPath,"staging")
+    const commitsPath = path.join(repoPath,"commits")
+    try{
+       
+        const commitID = uuidv4();
+        const commitDir = path.join(commitsPath,commitID)
+        await fs.mkdir(commitDir,{ recursive: true })
+
+        const files = await fs.readdir(stagingPath)
+        for(const file of files){
+            await fs.copyFile(
+                path.join(stagingPath,file),
+                path.join(commitDir,file)
+            )
+        }
+        await fs.writeFile(path.join(commitDir,"commit.json"),JSON.stringify({message,date:new Date().toISOString()}))
+        console.log(`Committed changes as ${commitID} with message: "${message}"`);
+    }catch(err){
+        console.error("Error during commit:", err);
+    }
+
 }
